82286f7d92318900bce7ad1c5530d141518e0ea2
masum <mr.razamasum@gmail.com>
2023-01-27 14:23:31 +0530

    Added files

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const randomWords = require('random-words');
 
 const app=express()
 
+app.use(express.json())
+app.use(cors({
+    origin:"*"
+}))
+
 app.get("/randomword",(req,res)=>{
     res.send(str=randomWords())
 
